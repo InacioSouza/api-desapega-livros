2eb3823bd1a0147584ec1ffcbb3461f8761f791c
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2024-11-13 10:44:47 -0300

    CategoriaService criado, não testado

--- src/main/java/br/com/livraria/controller/dto/CategoriaDTO.java
+++ src/main/java/br/com/livraria/controller/dto/CategoriaDTO.java
@@ -1,9 +1,12 @@
 package br.com.livraria.controller.dto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import br.com.livraria.repository.entity.Categoria;
 
 public class CategoriaDTO {
-	
+
 	private Long id;
 
 	private String nome;
@@ -20,12 +23,21 @@ public class CategoriaDTO {
 		return new CategoriaDTO(categoria.getId(), categoria.getNome());
 	}
 
+	public CategoriaDTO(Categoria categoria) {
+		this.id = categoria.getId();
+		this.nome = categoria.getNome();
+	}
+
 	public String getNome() {
 		return nome;
 	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<CategoriaDTO> converteParaCategoriaDTO(List<Categoria> categorias) {
+		return categorias.stream().map(CategoriaDTO::new).collect(Collectors.toList());
 	}
 
 }
--- src/main/java/br/com/livraria/services/CategoriaService.java
+++ src/main/java/br/com/livraria/services/CategoriaService.java
@@ -1,8 +1,58 @@
 package br.com.livraria.services;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import br.com.livraria.controller.dto.CategoriaDTO;
+import br.com.livraria.controller.form.CategoriaForm;
+import br.com.livraria.repository.CategoriaRepository;
+import br.com.livraria.repository.entity.Categoria;
+import br.com.livraria.services.response.Response;
 
 @Service
 public class CategoriaService {
+	
+	public ResponseEntity<?> cadastraCategoria(CategoriaForm categoriaForm,
+			CategoriaRepository categoriaRepo) {
+		if (categoriaForm == null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Response("Categoria null", 400));
+		} else if (categoriaForm.getNome() == null || categoriaForm.getNome().isEmpty()) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new Response("Nome da categoria não especificado", 400));
+		}
+		
+		Categoria categoria = null;
+		
+		categoria = categoriaRepo.findByNome(categoriaForm.getNome());
+		
+		if (categoria != null) {
+			return ResponseEntity.status(HttpStatus.CONFLICT)
+					.body(new Response("Categoria presente no banco de dados", 409));
+		}
+		
+		categoria = categoriaRepo.save(categoriaForm.converteParaCategoria());
+		
+		if (categoria == null) {
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+					.body(new Response("Categoria não cadastrada", 422));
+		}
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(new Response("Categoria cadastrada", 201));
+	}
+	
+	public ResponseEntity<?> listaCategorias(CategoriaRepository categoriaRepo) {
+
+		List<Categoria> categorias = (List<Categoria>) categoriaRepo.findAll();
+
+		if (categorias.size() == 0) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new Response("Nenhuma categoria encontrada", 404));
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).body(new CategoriaDTO().converteParaCategoriaDTO(categorias));
+
+	}
 
 }
